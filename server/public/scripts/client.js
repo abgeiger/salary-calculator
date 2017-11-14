92792ed5d18952454c97b7d232b329649afae426
abgeiger <30698092+abgeiger@users.noreply.github.com>
2017-11-14 17:00:12 -0600

    added server

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,9 +1,10 @@
-console.log('script.js sourced')
-var monthlyCosts = 0;
+console.log('script.js sourced');
+var indexCount = -1;
 
 $(document).ready(readyNow);
 
 function readyNow() {
+    var indexCount = 0;
     console.log('jQuery sourced');
     $('#submitButton').on('click', submitButtonClick);
     $('tbody').on('click', '#deleteButton', deleteRow)
@@ -15,14 +16,21 @@ function submitButtonClick() {
     var $idNumber = $('#idNumberInput').val();
     var $jobTitle = $('#jobTitleInput').val();
     var $annualSalary = $('#annualSalaryInput').val();
-    monthlyCosts += $annualSalary / 12;
+    var monthlyCost = $annualSalary / 12;
+    indexCount++;
     $('input').val('');
     $('table').append(
         '<tr><td>' + $firstName + '</td><td>' + $lasttName + '</td><td>' + $idNumber + '</td><td>' + $jobTitle + 
-        '</td><td>' + $annualSalary + '</td><td>' + monthlyCosts.toFixed(2) + 
+        '</td><td>' + $annualSalary + '</td><td data-index="' + indexCount + '">' + monthlyCost.toFixed(2) + 
         '</td><td><button id="deleteButton">Delete</button</td></tr>');
+    updateMonthlyCost();
+
 }
 
 function deleteRow() {
     $(this).parent().parent().remove();
+}
+
+function updateMonthlyCost() {
+    $('#monthlyCost').html('Total Monthly Cost: ' + totalMonthlyCost)
 }
